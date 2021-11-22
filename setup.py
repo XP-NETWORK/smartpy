1d03707a83461b39091083dda863c899f03b1ce9
rocalex <rocmaildev@gmail.com>
2021-11-22 12:03:59 +0300

    add readme

--- setup.py
+++ setup.py
@@ -1,41 +1,24 @@
-# Copyright (C) 2021  Xueming Zheng
-from setuptools import setup, __version__
+from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
-    long_desc = fh.read()
-
-with open('smartpy/version.py') as fv:
-    exec(fv.read())
+VERSION = '0.0.1'
+DESCRIPTION = 'SmartPy Python package'
+LONG_DESCRIPTION = 'SmartPy Python package'
 
 setup(
     name='smartpy',
-
-    version=__version__,
-
-    description='SMARTpy: an open-source rainfall-runoff model in Python',
-    long_description=long_desc,
-    long_description_content_type="text/markdown",
-
-    url='https://github.com/xp-network/smartpy',
-
-    author='Thibault Hallouin, Eva Mockler, and Michael Bruen',
-    author_email='thibault.hallouin@ucdconnect.ie',
-
-    license='GPLv3',
-
+    version=VERSION,
+    author='Xueming Zheng',
+    author_email='rocalex0220@outlook.com',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['smartpy'],
     classifiers=[
-        'Natural Language :: English',
-
-        'Operating System :: MacOS',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX :: Linux',
-
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
-
-    packages=['smartpy'],
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
 )
